3e2072fa46670ce2913b9d449f22dc9a5d478138
comfuture <comfuture@gmail.com>
2024-09-26 16:22:19 +0900

    change flutter lib directory exclude unnessary customizations simplify tests

--- src/flutter/install.sh
+++ src/flutter/install.sh
@@ -8,8 +8,8 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y curl git unzip xz-utils zip libglu1-mesa
 apt-get clean
-git clone https://github.com/flutter/flutter.git -b stable --depth 1 /flutter
-PATH="/flutter/bin:$PATH"
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 /usr/local/lib/flutter
+PATH="/usr/local/lib/flutter/bin:$PATH"
 flutter --android-sdk /usr/local/lib/android
 yes | flutter doctor --android-licenses
 flutter doctor
