26d2f6a762aef7cbb3f9b105d5f7c590e18ab67a
comfuture <comfuture@gmail.com>
2024-09-26 16:02:51 +0900

    Refactor Flutter installation script to include Android SDK path and accept licenses

--- src/flutter/install.sh
+++ src/flutter/install.sh
@@ -10,4 +10,6 @@ apt-get install -y curl git unzip xz-utils zip libglu1-mesa
 apt-get clean
 git clone https://github.com/flutter/flutter.git -b stable --depth 1 /flutter
 PATH="/flutter/bin:$PATH"
+flutter --android-sdk /usr/local/lib/android
+yes | flutter doctor --android-licenses
 flutter doctor
