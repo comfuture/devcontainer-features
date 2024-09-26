2efddbe27ef5a782a6933e5523f6d39d95a80e2d
comfuture <comfuture@gmail.com>
2024-09-26 15:02:27 +0900

    Initial commit
    - Added .devcontainer.json file to configure the development container with necessary extensions and features.
    - Added release.yml workflow file to automate the release process.
    - Created Flutter devcontainer template with necessary dependencies and settings.
    - Added install.sh script to install Flutter and its dependencies.

---
+++ src/flutter/README.md
@@ -0,0 +1,8 @@
+# Flutter Devcontainer Template
+
+This is a template for a Flutter development container feature. It includes the
+following:
+
+- Android Tool Chain
+- Dart
+- Flutter
---
+++ src/flutter/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -ue
+
+echo "Install Flutter"
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update
+apt-get install -y curl git unzip xz-utils zip libglu1-mesa
+apt-get clean
+git clone https://github.com/flutter/flutter.git -b stable --depth 1 /flutter
+PATH="/flutter/bin:$PATH"
+flutter doctor
